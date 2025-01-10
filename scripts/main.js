ac6698a38add0cb15e0fbb25a71ece930257dd9f
ondicheese <ondicheese@tutanota.com>
2025-01-10 13:00:34 +0100

    Deleting useless code

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,3 @@
-/*
-let writingUserArea = document.getElementById('inputEcriture')
-console.log(writingUserArea)
-
-let btnValidateWord = document.getElementById('btnValiderMot')
-console.log(btnValidateWord)
-
-let propositionsArea = document.querySelector('.zoneProposition')
-console.log(propositionsArea)
-
-let scoreArea = document.querySelector('.zoneScore')
-console.log(scoreArea)
-
-let picksRadioBtns = document.querySelectorAll('.optionSource input[type="radio"]')
-console.log(picksRadioBtns)
-*/
-/*
-test4 = document.getElementsByTagName("optionSource")
-test2 = document.getElementById("phrases")
-test3 = document.getElementById("mots")
-test = document.querySelector(".optionSource")
-console.log(test4)*/
-/*
-test.addEventListener('change', ()=>{
-    console.log(test4)
-
-})
-*/
-
 lancerJeu()
 
 
